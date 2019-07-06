489f43e45d1f3a7855b8418b2ee3eba073d350ba
Francesca Chater <francescachater@gmail.com>
2019-07-06 12:17:47 +0100

    egde case unit tests

--- spec/highest_number_spec.rb
+++ spec/highest_number_spec.rb
@@ -5,5 +5,29 @@ describe 'highest_number' do
         list = [1, 2, 3, 0, 3, 5]
         expect(highest_number(list)).to eq(5)
     end 
+
+    it 'returns the highest number from given array' do 
+        list = [-4, 10, -100, 1]
+        expect(highest_number(list)).to eq(10)
+    end 
+
+    it 'returns one number if array is duplicates' do 
+        list = [2, 2, 2]
+        expect(highest_number(list)).to eq(2)
+    end 
+
+    it 'returns number array length is 1' do 
+        list = [9]
+        expect(highest_number(list)).to eq(9)
+    end 
+
+    it 'returns empty array if array is empty' do 
+        list = []
+        expect(highest_number(list)).to eq([])
+    end 
+
+
+
+
 end 
 
