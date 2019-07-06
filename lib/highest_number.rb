99806cf409ecdbd7ce4b7ba2ae80d035b98bd433
Francesca Chater <francescachater@gmail.com>
2019-07-06 12:27:51 +0100

    passing tests for edge cases

--- lib/highest_number.rb
+++ lib/highest_number.rb
@@ -2,7 +2,8 @@
 # Comments left in to show examples of getting visibility
 
 def highest_number(list)
-  current_maximum = list[0]
+  list.empty? ? current_maximum = list :
+   current_maximum = list[0]
 
     list.each do | number | 
         # list = [1, 2, 3, 0, 3, 5]
