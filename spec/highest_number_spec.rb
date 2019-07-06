40d55d0f90103e422b1e16b3d9f171786d45c7cb
Francesca Chater <francescachater@gmail.com>
2019-07-06 13:39:59 +0100

    Remove redundant test

--- spec/highest_number_spec.rb
+++ spec/highest_number_spec.rb
@@ -2,16 +2,11 @@ require 'highest_number'
 
 describe 'highest_number' do 
   it 'returns the highest number from given array' do 
-    list = [1, 2, 3, 0, 3, 5]
+    list = [1, -10, 3, 0, 3, 5]
     expect(highest_number_in_list(list)).to eq(5)
   end 
 
-  it 'returns the highest number from given array' do 
-    list = [-4, 10, -100, 1]
-    expect(highest_number_in_list(list)).to eq(10)
-  end 
-
-  it 'returns one number if there are duplicates' do 
+  it 'returns one number if there are duplicates of highest number' do 
     list = [2, 2, 2]
     expect(highest_number_in_list(list)).to eq(2)
   end 
