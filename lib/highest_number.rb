1bd0566738a24411c0faa7f66bee9f86237b8ba0
Francesca Chater <francescachater@gmail.com>
2019-07-06 12:28:29 +0100

    Linter refactoring

--- lib/highest_number.rb
+++ lib/highest_number.rb
@@ -5,18 +5,18 @@ def highest_number(list)
   list.empty? ? current_maximum = list :
    current_maximum = list[0]
 
-    list.each do | number | 
-        # list = [1, 2, 3, 0, 3, 5]
-        if number > current_maximum
-          # "-------current maximum in loop------------"
-          current_maximum = number 
-          # p current_maximum 
-        else 
-          # p "----------number < current maximum in loop ----------"
-          number 
-        end 
-      # p "----------current maximum out of loop ----------"   
-      # p current_maximum
-    end 
+  list.each do |number| 
+      # list = [1, 2, 3, 0, 3, 5]
+    if number > current_maximum
+        # "-------current maximum in loop------------"
+      current_maximum = number 
+        # p current_maximum 
+      else 
+        # p "----------number < current maximum in loop ----------"
+        number 
+      end 
+    # p "----------current maximum out of loop ----------"   
+    # p current_maximum
+  end 
   current_maximum
-end 
+end
